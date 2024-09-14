3820e90673ee5e55fd200ec3b517f19411a8e59e
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-09-14 13:48:13 +0530

    add route protection

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import Single from "./pages/Single";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Certificates from "./pages/Certificates";
+import AuthRoute from "./utils/AuthRoute";
 
 const HeaderFooterWrapper = ({ children }) => {
   const location = useLocation();
@@ -32,13 +32,15 @@ function App() {
       <BrowserRouter>
         <HeaderFooterWrapper>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<AuthRoute element={Home} />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/single" element={<Single />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/certificates" element={<Certificates />} />
+            <Route path="/about" element={<AuthRoute element={About} />} />
+            <Route path="/contact" element={<AuthRoute element={Contact} />} />
+            <Route
+              path="/certificates"
+              element={<AuthRoute element={Certificates} />}
+            />
           </Routes>
         </HeaderFooterWrapper>
       </BrowserRouter>
